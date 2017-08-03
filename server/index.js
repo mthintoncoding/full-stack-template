b06c0aa3c6fc7143e2a857047c1211127e1baad2
mthintoncoding <mthinton@gmail.com>
2017-08-03 14:15:01 -0400

    added database schema

--- server/index.js
+++ server/index.js
@@ -1,19 +1,58 @@
 const path = require('path');
-const Mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const {Client} = require('./clientmodel');
 
+mongoose.Promise = global.Promise;
 
 const app = express();
 
 // API endpoints go here!
 
-
-
 // Serve the built client
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get('/api/test', (req, res) => {
-    res.json({message: 'hello'})
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+
+app.post('/api/login', (req, res) => {
+    return Client
+    .find({username})
+    .count()
+    .exec()
+    .then(count => {
+        if(count > 0){
+            return Promise.reject({
+                name: 'Authentication Error',
+                message: 'username already taken'
+            });
+        }
+        return Client.hashPassword(password);
+    })
+    .then(hash => {
+        return Client
+            .create({
+                username: username,
+                password: hash,
+                email: email
+            })
+    })
+    .then(client => {
+        return res.status(201).json(client);
+    })
+    .catch(err => {
+        res.status(500).json({message: 'Internal server error'})
+    });
+});
+
+app.get('/api/login', (req, res) => {
+    return Client
+    .find()
+    .exec()
+    .then(users => res.json(users))
+    .catch(err => console.log(err) && res.status(500).json({message: 'Internal server error'}));
 })
 
 // Unhandled requests which aren't for the API should serve index.html so
@@ -26,10 +65,20 @@ app.get(/^(?!\/api(\/|$))/, (req, res) => {
 let server;
 function runServer(port=3001) {
     return new Promise((resolve, reject) => {
-        server = app.listen(port, () => {
+        mongoose.connect('mongodb://Barber:1234Europe!@ds157631.mlab.com:57631/barbershopclients', err => {
+            if(err){
+                return reject(err);
+            }
+
+            server = app.listen(port, () => {
             resolve();
-        }).on('error', reject);
-    });
+            })
+            .on('error', err => {
+                mongoose.disconnect();
+                reject(err);
+            });
+        });
+    })
 }
 
 function closeServer() {
